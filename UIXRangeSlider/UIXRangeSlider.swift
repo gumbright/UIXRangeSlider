b3d44479acd220f0fbccf5af26452d568ba98549
Guy Umbright <gumbright@gmail.com>
2015-02-15 17:14:19 -0600

    adding for remote

---
+++ UIXRangeSlider/UIXRangeSlider.swift
@@ -0,0 +1,343 @@
+//
+//  UIXRangeSlider.swift
+//  UIXRangeSlider
+//
+//  Created by Guy Umbright on 2/12/15.
+//  Copyright (c) 2015 Umbright Consulting, Inc. All rights reserved.
+//
+
+import UIKit
+
+class UIXRangeSlider: UIControl
+{
+    //Config
+    var leftThumbImage:UIImage = UIImage()
+    var rightThumbImage:UIImage = UIImage()
+    var thumbActiveBarInset = 0.0
+    
+    var middleThumbImage:UIImage = UIImage()
+    
+    var inactiveBarImage:UIImage?
+    //var inactiveBarImageCapInsets:UIEdgeInsets = UIEdgeInsetsZero
+    
+    var activeBarImage:UIImage = UIImage()
+    //var activeBarImageCapInsets:UIEdgeInsets = UIEdgeInsetsZero
+    
+    //State
+    var minimumValue:Double = 0.0
+    var maximumValue:Double = 100.0
+    var leftValue:Double = 30.0
+    var rightValue:Double = 70.0
+    
+    //component views
+    var inactiveBarView:UIImageView = UIImageView()
+    var activeBarView:UIImageView = UIImageView()
+    var leftThumbView:UIImageView = UIImageView()
+    var rightThumbView:UIImageView = UIImageView()
+    var middleThumbView:UIImageView = UIImageView()
+    
+    var leftPanGestureRecognizer:UIPanGestureRecognizer?
+    var rightPanGestureRecognizer:UIPanGestureRecognizer?
+    var middlePanGestureRecognizer:UIPanGestureRecognizer?
+    
+    //min ht = 31
+    //respect view width, height is centered on view y
+    
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+    /////////////////////////////////////////////////////
+    //
+    /////////////////////////////////////////////////////
+    func commonInit()
+    {
+        self.leftPanGestureRecognizer = UIPanGestureRecognizer(target: self, action: Selector("handleLeftPan:"))
+        self.rightPanGestureRecognizer = UIPanGestureRecognizer(target: self, action: Selector("handleRightPan:"))
+        self.middlePanGestureRecognizer = UIPanGestureRecognizer(target: self, action: Selector("handleMiddlePan:"))
+    }
+    
+    /////////////////////////////////////////////////////
+    //
+    /////////////////////////////////////////////////////
+    override init(frame: CGRect)
+    {
+        super.init(frame: frame)
+        self.commonInit()
+    }
+
+    /////////////////////////////////////////////////////
+    //
+    /////////////////////////////////////////////////////
+    required init(coder: NSCoder)
+    {
+        super.init(coder: coder)
+        self.commonInit()
+    }
+    
+    /////////////////////////////////////////////////////
+    //
+    /////////////////////////////////////////////////////
+    override func awakeFromNib()
+    {
+        self.commonInit()
+    }
+    
+    /////////////////////////////////////////////////////
+    //
+    /////////////////////////////////////////////////////
+    override func layoutSubviews()
+    {
+        let viewCenter = self.convertPoint(self.center, fromView: self.superview)
+        self.inactiveBarView.center.y = viewCenter.y
+        var frame = self.inactiveBarView.frame
+        frame.origin.x = self.leftThumbView.frame.width
+        frame.size.width = self.bounds.width - (self.leftThumbView.bounds.width + self.rightThumbView.bounds.width)
+        self.inactiveBarView.frame = frame
+        
+        self.leftThumbView.center.y = viewCenter.y
+        self.positionLeftThumb()
+        self.rightThumbView.center.y = viewCenter.y
+        self.positionRightThumb()
+        
+        self.middleThumbView.center.y = viewCenter.y
+        frame = self.middleThumbView.frame
+        frame.origin.x = CGRectGetMaxX(self.leftThumbView.frame)
+        frame.size.width = CGRectGetMinX(self.rightThumbView.frame) - frame.origin.x
+        self.middleThumbView.frame = frame
+        
+        self.activeBarView.center.y = viewCenter.y
+        frame = self.activeBarView.frame
+        frame.origin.x = CGRectGetMaxX(self.leftThumbView.frame)
+        frame.size.width = CGRectGetMinX(self.rightThumbView.frame) - frame.origin.x
+        self.activeBarView.frame = frame
+    }
+    
+    /////////////////////////////////////////////////////
+    //
+    /////////////////////////////////////////////////////
+    func positionLeftThumb()
+    {
+        let pos = CGFloat(positionForValue(self.leftValue))
+        var frame = self.leftThumbView.frame
+        frame.origin.x = CGFloat(pos - self.leftThumbView.bounds.width)
+        self.leftThumbView.frame = frame;
+    }
+    
+    /////////////////////////////////////////////////////
+    //
+    /////////////////////////////////////////////////////
+    func positionRightThumb()
+    {
+        let pos = CGFloat(positionForValue(self.rightValue))
+        var frame = self.rightThumbView.frame
+        frame.origin.x = CGFloat(pos)
+        self.rightThumbView.frame = frame;
+    }
+    
+    /////////////////////////////////////////////////////
+    //
+    /////////////////////////////////////////////////////
+    func positionForValue(value:Double) -> Double
+    {
+        return (Double(self.inactiveBarView.frame.width) * (value - self.minimumValue) / (self.maximumValue - self.minimumValue)) +  Double(self.inactiveBarView.frame.origin.x)
+    }
+    
+    /////////////////////////////////////////////////////
+    //
+    /////////////////////////////////////////////////////
+    func setInactiveBarImage(inactiveBarImage:UIImage)
+    {
+        self.inactiveBarImage = inactiveBarImage
+        
+        let newView = UIImageView(image: self.inactiveBarImage)
+        self.inactiveBarView.removeFromSuperview()
+        self.inactiveBarView = UIImageView(image: self.inactiveBarImage)
+        self.addSubview(self.inactiveBarView)
+        self.orderSubviews()
+        self.setNeedsLayout()
+    }
+    
+    /////////////////////////////////////////////////////
+    //
+    /////////////////////////////////////////////////////
+    func setActiveBarImage(activeBarImage:UIImage)
+    {
+        self.activeBarImage = activeBarImage
+        self.activeBarView.removeFromSuperview()
+        self.activeBarView = UIImageView(image: self.activeBarImage)
+        self.addSubview(self.activeBarView)
+        self.orderSubviews()
+        self.setNeedsLayout()
+    }
+    
+    /////////////////////////////////////////////////////
+    //
+    /////////////////////////////////////////////////////
+    func setLeftThumbImage(leftThumbImage:UIImage)
+    {
+        self.leftThumbImage = leftThumbImage
+        self.leftThumbView.removeGestureRecognizer(self.leftPanGestureRecognizer!)
+        self.leftThumbView.removeFromSuperview()
+        self.leftThumbView = UIImageView(image: self.leftThumbImage)
+        self.leftThumbView.userInteractionEnabled = true
+        self.leftThumbView.addGestureRecognizer(self.leftPanGestureRecognizer!)
+        self.addSubview(self.leftThumbView)
+        self.orderSubviews()
+        self.setNeedsLayout()
+    }
+
+    /////////////////////////////////////////////////////
+    //
+    /////////////////////////////////////////////////////
+    func setRightThumbImage(rightThumbImage:UIImage)
+    {
+        self.rightThumbImage = rightThumbImage
+        self.rightThumbView.removeGestureRecognizer(self.rightPanGestureRecognizer!)
+        self.rightThumbView.removeFromSuperview()
+        self.rightThumbView = UIImageView(image: self.rightThumbImage)
+        self.rightThumbView.userInteractionEnabled = true
+        self.rightThumbView.addGestureRecognizer(self.rightPanGestureRecognizer!)
+        self.addSubview(self.rightThumbView)
+        self.orderSubviews()
+        self.setNeedsLayout()
+    }
+    
+    /////////////////////////////////////////////////////
+    //
+    /////////////////////////////////////////////////////
+    func setMiddleThumbImage(middleThumbImage:UIImage)
+    {
+        self.middleThumbImage = middleThumbImage
+        self.middleThumbView.removeGestureRecognizer(self.middlePanGestureRecognizer!)
+        self.middleThumbView.removeFromSuperview()
+        self.middleThumbView = UIImageView(image: self.middleThumbImage)
+        self.middleThumbView.userInteractionEnabled = true
+        self.middleThumbView.addGestureRecognizer(self.middlePanGestureRecognizer!)
+        self.addSubview(self.middleThumbView)
+        self.orderSubviews()
+        self.setNeedsLayout()
+    }
+    
+    /////////////////////////////////////////////////////
+    //
+    /////////////////////////////////////////////////////
+    func orderSubviews()
+    {
+        for view in self.subviews as [UIView]
+        {
+            view.removeFromSuperview()
+        }
+        
+        self.addSubview(self.inactiveBarView)
+        self.addSubview(self.leftThumbView)
+        self.addSubview(self.middleThumbView)
+        self.addSubview(self.rightThumbView)
+        self.addSubview(self.activeBarView)
+    }
+    
+    
+    /////////////////////////////////////////////////////
+    //
+    /////////////////////////////////////////////////////
+    func handleLeftPan(gestureRecognizer: UIPanGestureRecognizer)
+    {
+        if (gestureRecognizer.state == UIGestureRecognizerState.Began || gestureRecognizer.state == UIGestureRecognizerState.Changed)
+        {
+            let translation = gestureRecognizer.translationInView(self)
+            let range = self.maximumValue - self.minimumValue
+            let availableWidth = self.inactiveBarView.frame.width
+            
+            let newValue = self.leftValue + Double(translation.x) / Double(availableWidth) * range
+           
+            self.leftValue = newValue
+            
+            if (self.leftValue < minimumValue)
+            {
+                self.leftValue = minimumValue
+            }
+            
+            if (self.leftValue > self.rightValue)
+            {
+                self.leftValue = self.rightValue
+            }
+            
+            gestureRecognizer.setTranslation(CGPointZero, inView: self)
+            self.setNeedsLayout()
+            self.sendActionsForControlEvents(UIControlEvents.ValueChanged)
+        }
+    }
+    
+     /////////////////////////////////////////////////////
+    //
+    /////////////////////////////////////////////////////
+    func handleRightPan(gestureRecognizer: UIPanGestureRecognizer)
+    {
+        if (gestureRecognizer.state == UIGestureRecognizerState.Began || gestureRecognizer.state == UIGestureRecognizerState.Changed)
+        {
+            let translation = gestureRecognizer.translationInView(self)
+            let range = self.maximumValue - self.minimumValue
+            let availableWidth = self.inactiveBarView.frame.width
+            let newValue = self.rightValue + Double(translation.x) / Double(availableWidth) * range
+
+            self.rightValue = newValue
+
+
+            if (self.rightValue > self.maximumValue)
+            {
+                self.rightValue = self.maximumValue
+            }
+            
+            if (self.rightValue < self.leftValue)
+            {
+                self.rightValue = self.leftValue
+            }
+
+            gestureRecognizer.setTranslation(CGPointZero, inView: self)
+            self.setNeedsLayout()
+            self.sendActionsForControlEvents(UIControlEvents.ValueChanged)
+        }
+    }
+    
+    /////////////////////////////////////////////////////
+    //
+    /////////////////////////////////////////////////////
+    func handleMiddlePan(gestureRecognizer: UIPanGestureRecognizer)
+    {
+        if (gestureRecognizer.state == UIGestureRecognizerState.Began || gestureRecognizer.state == UIGestureRecognizerState.Changed)
+        {
+            let translation = gestureRecognizer.translationInView(self)
+            let range = self.maximumValue - self.minimumValue
+            let availableWidth = self.inactiveBarView.frame.width
+            let diff = self.rightValue - self.leftValue
+            
+            let newLeftValue = self.leftValue + Double(translation.x) / Double(availableWidth) * range
+            if (newLeftValue < minimumValue)
+            {
+                self.leftValue = self.minimumValue
+                self.rightValue = self.leftValue + diff
+            }
+            else
+            {
+                let newRightValue = self.rightValue + Double(translation.x) / Double(availableWidth) * range
+                if (newRightValue > self.maximumValue)
+                {
+                    self.rightValue = self.maximumValue
+                    self.leftValue = self.rightValue - diff
+                }
+                else
+                {
+                    self.leftValue = newLeftValue
+                    self.rightValue = self.leftValue + diff
+                }
+            }
+            gestureRecognizer.setTranslation(CGPointZero, inView: self)
+            self.setNeedsLayout()
+            self.sendActionsForControlEvents(UIControlEvents.ValueChanged)
+        }
+    }
+}
---
+++ UIXRangeSliderExample/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  UIXRangeSlider
+//
+//  Created by Guy Umbright on 2/12/15.
+//  Copyright (c) 2015 Umbright Consulting, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var mySlider:UIXRangeSlider?
+    @IBOutlet var recogView:UIView?
+    @IBOutlet var leftValue:UILabel?
+    @IBOutlet var rightValue:UILabel?
+    
+    var recog:UIPanGestureRecognizer?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let inactiveBarImage = UIImage(named: "inactivebar")
+        let ibi2 = inactiveBarImage?.resizableImageWithCapInsets(UIEdgeInsetsMake(0, 5, 0, 5))
+        self.mySlider?.setInactiveBarImage(ibi2!)
+        
+        let activeBarImage = UIImage(named: "activebar")
+        let abi = activeBarImage?.resizableImageWithCapInsets(UIEdgeInsetsZero)
+        self.mySlider?.setActiveBarImage(abi!)
+        
+        self.mySlider?.setLeftThumbImage(UIImage(named: "leftthumb")!)
+        self.mySlider?.setRightThumbImage(UIImage(named: "rightthumb")!)
+        self.mySlider?.setMiddleThumbImage(UIImage(named: "middlethumb")!.resizableImageWithCapInsets(UIEdgeInsetsZero))
+        
+        self.recog = UIPanGestureRecognizer(target: self, action: "tapped:")
+        recogView?.addGestureRecognizer(self.recog!)
+    }
+    
+    func tapped(recog:UIPanGestureRecognizer)
+    {
+        println("tapped")
+        recogView?.removeGestureRecognizer(self.recog!)
+        recogView?.addGestureRecognizer(self.recog!)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func sliderChanged(slider:UIXRangeSlider)
+    {
+        self.leftValue?.text = "\(slider.leftValue)"
+        self.rightValue?.text = "\(slider.rightValue)"
+    }
+}
+
